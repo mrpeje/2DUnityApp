198546baa629ad49457319f907393d07d820a4bb
Belosorochko Yury <whond.earn.war@gmail.com>
2022-03-27 15:09:21 +0300

    Code refactoring

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -1,45 +1,44 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
     public GameObject map;
-    private float mapMinX, mapMaxX, mapMinY, mapMaxY;
-    private Vector3 originPos, newPos;
-    private Vector2 delta1, delta2;
-    public float distance, distance2;
+    private float mapMinX, mapMaxX, mapMinY, mapMaxY; 
+    private Vector3 originCamPos, newCamPos; 
     private float zoomStep, minCamSize, maxCamSize;
 
-    // Start is called before the first frame update
+    // Init
     private void Awake()
     {
         SpriteRenderer sp = map.GetComponent<SpriteRenderer>();
+
         mapMinX = sp.transform.position.x - sp.bounds.size.x / 2f;
         mapMaxX = sp.transform.position.x + sp.bounds.size.x / 2f;
 
         mapMinY = sp.transform.position.y - sp.bounds.size.y / 2f;
         mapMaxY = sp.transform.position.y + sp.bounds.size.y / 2f;
-        distance = 0;
 
-        zoomStep = 1/10f;
+        zoomStep = 1/20f;
         minCamSize = 1;
         maxCamSize = mapMaxY/2;
     }
     void ZoomIn()
     {
-        float newSize = Camera.main.orthographicSize - zoomStep;
-        Camera.main.orthographicSize = Mathf.Clamp(newSize, minCamSize, maxCamSize);
-
-        Camera.main.transform.position = ClampCamera(Camera.main.transform.position);   //ograniczenie obszaru
-    }
-    void ZoomOut()
-    {
         float newSize = Camera.main.orthographicSize + zoomStep;
         Camera.main.orthographicSize = Mathf.Clamp(newSize, minCamSize, maxCamSize);
 
-        Camera.main.transform.position = ClampCamera(Camera.main.transform.position);   //ograniczenie obszaru
+        Camera.main.transform.position = ClampCamera(Camera.main.transform.position);   
     }
+    void ZoomOut()
+    {
+        float newSize = Camera.main.orthographicSize - zoomStep;
+        Camera.main.orthographicSize = Mathf.Clamp(newSize, minCamSize, maxCamSize);
+
+        Camera.main.transform.position = ClampCamera(Camera.main.transform.position);   
+    }
+    // Camera view clamping
     Vector3 ClampCamera(Vector3 pos)
     {
         float cameraHeight = Camera.main.orthographicSize * 2f; // orthographicSize is half of camera height
@@ -54,15 +53,17 @@ public class CameraControl : MonoBehaviour
 
         return new Vector3(Mathf.Clamp(pos.x, minX, maxX), Mathf.Clamp(pos.y, minY, maxY), pos.z);
     }
-    // Update is called once per frame
     void Update()
     {
-
+        // ZoomIn and ZoomOut
         if (Input.touchCount == 2)
         {
-               
+            float distance, distance2;
+            Vector2 delta1, delta2;
+
             Touch touch1 = Input.GetTouch(0);
             Touch touch2 = Input.GetTouch(1);
+
             if (touch1.phase == TouchPhase.Moved || touch2.phase == TouchPhase.Moved)
             {
                 delta1 = touch1.deltaPosition;
@@ -75,16 +76,17 @@ public class CameraControl : MonoBehaviour
                     ZoomOut();
             }
         }
+        //Drag camera
         if (Input.touchCount < 2)
         {
             if (Input.GetMouseButtonDown(0))
             {
-                originPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                originCamPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             }
             if (Input.GetMouseButton(0))
             {
-                newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                Camera.main.transform.position = ClampCamera(Camera.main.transform.position + originPos - newPos);
+                newCamPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Camera.main.transform.position = ClampCamera(Camera.main.transform.position + originCamPos - newCamPos);
             }
         }
     }
