a634c77c155a1f1ec167dd219e7414886b6f73ea
Belosorochko Yury <whond.earn.war@gmail.com>
2022-04-04 21:24:43 +0300

    Adding all maps and buttons associated with them

--- Assets/Scripts/UIcontroller.cs
+++ Assets/Scripts/UIcontroller.cs
@@ -7,7 +7,7 @@ public class UIcontroller : MonoBehaviour
 {
     public MapGroup Maps;
     public Button MenuBtn;
-    public Button WoodsBtn, CustomsBtn;
+    public Button WoodsBtn, CustomsBtn, FactoryBtn, ReserveBtn, ShorelineBtn, LighthouseBtn, InterchangeBtn;
     bool IsMenuOpen;
   
     void OpenMenu()
@@ -17,6 +17,12 @@ public class UIcontroller : MonoBehaviour
         {
             WoodsBtn.style.display = DisplayStyle.Flex;
             CustomsBtn.style.display = DisplayStyle.Flex;
+            FactoryBtn.style.display = DisplayStyle.Flex;
+            ReserveBtn.style.display = DisplayStyle.Flex;
+            ShorelineBtn.style.display = DisplayStyle.Flex;
+            LighthouseBtn.style.display = DisplayStyle.Flex;
+            InterchangeBtn.style.display = DisplayStyle.Flex;
+
             IsMenuOpen = true;
         }
         // Close menu
@@ -24,6 +30,12 @@ public class UIcontroller : MonoBehaviour
         {
             WoodsBtn.style.display = DisplayStyle.None;
             CustomsBtn.style.display = DisplayStyle.None;
+            FactoryBtn.style.display = DisplayStyle.None;
+            ReserveBtn.style.display = DisplayStyle.None;
+            ShorelineBtn.style.display = DisplayStyle.None;
+            LighthouseBtn.style.display = DisplayStyle.None;
+            InterchangeBtn.style.display = DisplayStyle.None;
+
             IsMenuOpen = false;
         }
     }
@@ -32,22 +44,55 @@ public class UIcontroller : MonoBehaviour
         IsMenuOpen = true;
         var root = GetComponent<UIDocument>().rootVisualElement;
         MenuBtn = root.Q<Button>("MapSelectionMenu");
+
         WoodsBtn = root.Q<Button>("Woods");
         CustomsBtn = root.Q<Button>("Customs");
+        FactoryBtn = root.Q<Button>("Factory");
+        ReserveBtn = root.Q<Button>("Reserve");
+        ShorelineBtn = root.Q<Button>("Shoreline");
+        LighthouseBtn = root.Q<Button>("Lighthouse");
+        InterchangeBtn = root.Q<Button>("Interchange");
 
         MenuBtn.clicked += OpenMenu;
         WoodsBtn.clicked += WoodsActive;
         CustomsBtn.clicked += CustomsActive;
+        FactoryBtn.clicked += FactoryActive;
+        ReserveBtn.clicked += ReserveActive;
+        ShorelineBtn.clicked += ShorelineActive;
+        LighthouseBtn.clicked += LighthouseActive;
+        InterchangeBtn.clicked += InterchangeActive;
 
-    }
-    void WoodsActive()
-    {
-        Maps.SetActive(1);
     }
 
     void CustomsActive()
     {
         Maps.SetActive(0);
     }
+    void FactoryActive()
+    {
+        Maps.SetActive(1);
+    }
+    void LighthouseActive()
+    {
+        Maps.SetActive(2);
+    }
+    void WoodsActive()
+    {
+        Maps.SetActive(3);
+    }
+    void InterchangeActive()
+    {
+        Maps.SetActive(5);
+    }
+    void ReserveActive()
+    {
+        Maps.SetActive(4);
+    }
+    void ShorelineActive()
+    {
+        Maps.SetActive(6);
+    }
+
+
 
 }
